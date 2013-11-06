2b07c9da4393197fbf6cfd27c76e707dd97e26d4
evilkhaoskat <evilkhaoskat@gmail.com>
2013-11-06 18:36:32 +0400

    idea how to fix logical error

--- eight_queens_one/src/main/java/ru/eight_queens/logic/position_choose/choosers/PositionChooser.java
+++ eight_queens_one/src/main/java/ru/eight_queens/logic/position_choose/choosers/PositionChooser.java
@@ -10,6 +10,10 @@ import ru.eight_queens.entities.Field;
  * Time: 14:01
  * To change this template use File | Settings | File Templates.
  */
-public interface PositionChooser {
+public interface PositionChooser
+{
+    //TODO to solve the logical problem need to add list of already chosen variants for that particular figure, maybe save it in figure as it
+    //as result we will have full combination of all used position in figure, if there is no any other variants -
+    //try to set in another place previous queen
     public Cell getSuitablePosition(Field field);
 }
